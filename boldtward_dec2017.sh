1088da5ceac4271c0dfc3a072e975d5be715a385
Daniel Tward <daniel.tward@gmail.com>
2019-12-04 13:37:17 -0500

    bold

---
+++ boldtward_dec2017.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FNAME=Tward_CV_Complete_Dec2019.bbl
+sed -i -e 's/Tward/\\textbf{Tward}/g' $FNAME
+sed -i -e 's/giveni={D\\bibinitperiod\\bibinitdelim J\\bibinitperiod}/giveni={\\textbf{D\\bibinitperiod\\bibinitdelim J\\bibinitperiod}}/g' $FNAME
