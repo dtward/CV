2d279220c25a23169723f0bcc7ed56cac880771f
dtward <dtward@cis.jhu.edu>
2019-12-13 14:06:45 -0500

    done dec 2017

--- boldtward_dec2017.sh
+++ boldtward_dec2017.sh
@@ -2,4 +2,5 @@
 
 FNAME=Tward_CV_Complete_Dec2019.bbl
 sed -i -e 's/Tward/\\textbf{Tward}/g' $FNAME
+sed -i -e 's/D.~J./\\textbf{D.~J.}/g' $FNAME
 sed -i -e 's/giveni={D\\bibinitperiod\\bibinitdelim J\\bibinitperiod}/giveni={\\textbf{D\\bibinitperiod\\bibinitdelim J\\bibinitperiod}}/g' $FNAME
