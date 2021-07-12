a15c8f9f0ee56fcbcda4e96ad2481ac5f06e6a7a
Daniel Tward <daniel.tward@gmail.com>
2021-07-12 09:50:56 -0700

    about to update july 2021

--- boldtward.sh
+++ boldtward.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 FNAME=Tward_CV_Complete_Jul2019_ieee.bbl
+FNAME=Tward_CV_Complete_July2020.bbl
+FNAME=Tward_CV_Complete_August2020.bbl
+FNAME=Tward_CV_Complete_February2021.bbl
+#FNAME=Tward_CV_Complete_July2020_selected_for_xsede.bbl
+
 sed -i -e 's/Tward/\\textbf{Tward}/g' $FNAME
 sed -i -e 's/giveni={D\\bibinitperiod\\bibinitdelim J\\bibinitperiod}/giveni={\\textbf{D\\bibinitperiod\\bibinitdelim J\\bibinitperiod}}/g' $FNAME
